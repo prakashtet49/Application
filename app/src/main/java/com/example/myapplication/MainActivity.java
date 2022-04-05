40fdff041311bfb56c495cbe94d118de6838cebb
prakasht <prakashtet49@gmail.com>
2022-04-05 10:41:03 +0530

    First Draft

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView t1,t2;
+    int switch1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1=findViewById(R.id.textview_1);
+        t2=findViewById(R.id.textview_2);
+
+    }
+
+
+
+
+
+
+}
